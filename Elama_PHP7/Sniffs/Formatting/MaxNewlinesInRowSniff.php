abb4ea226655b3d25d68e9befd5255db41a3ab76
GeorgeBekh <georgebekh@mail.ru>
2017-01-10 14:25:23 +0300

    1 пустая строка

--- Elama_PHP7/Sniffs/Formatting/MaxNewlinesInRowSniff.php
+++ Elama_PHP7/Sniffs/Formatting/MaxNewlinesInRowSniff.php
@@ -3,7 +3,7 @@
 class Elama_PHP7_Sniffs_Formatting_MaxNewlinesInRowSniff implements PHP_CodeSniffer_Sniff
 {
 
-    public $maxEmptyLinesInRow = 2;
+    public $maxEmptyLinesInRow = 1;
 
     /**
      * Returns an array of tokens this test wants to listen for.
