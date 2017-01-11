4180b582ee7fe53e1676b5fa8960c3c5df5bd327
GeorgeBekh <georgebekh@mail.ru>
2017-01-11 18:57:27 +0300

    fix

--- scripts/run-codesniffer.sh
+++ scripts/run-codesniffer.sh
@@ -5,13 +5,12 @@ git fetch origin master:master
 fork_point=$(git merge-base --octopus master)
 FILES=$(git diff --diff-filter=AMRC --name-only ${fork_point} | grep .php | tr "\n" " ")
 
-
 WORKDIR=$( pwd )
+
 cd $( dirname "$0" )
+
 BIN_DIR=$( pwd )
-
 PHP_VERSION=$( php -r 'echo PHP_MAJOR_VERSION;' )
-
 IGNORED_DIRS=${1}
 
 if [ "$FILES" != "" ]
