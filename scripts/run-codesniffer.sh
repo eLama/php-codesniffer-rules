61820008652bcb58c8b88889fd117161e0a4f2de
GeorgeBekh <georgebekh@mail.ru>
2017-01-11 18:55:24 +0300

    fix

--- scripts/run-codesniffer.sh
+++ scripts/run-codesniffer.sh
@@ -5,26 +5,25 @@ git fetch origin master:master
 fork_point=$(git merge-base --octopus master)
 FILES=$(git diff --diff-filter=AMRC --name-only ${fork_point} | grep .php | tr "\n" " ")
 
-PHP_VERSION=$( php -r 'echo PHP_MAJOR_VERSION;' )
-ROOT_DIR = $( cd .. && echo ${PWD##*/} )
 
-if [ ${ROOT_DIR} -eq 'vendor' ]
-then
-    VENDOR_DIR=..
-else
-    VENDOR_DIR=vendor
-fi
+WORKDIR=$( pwd )
+cd $( dirname "$0" )
+BIN_DIR=$( pwd )
+
+PHP_VERSION=$( php -r 'echo PHP_MAJOR_VERSION;' )
 
 IGNORED_DIRS=${1}
 
 if [ "$FILES" != "" ]
 then
-    ./phpcs -p -v
-    --standard=${VENDOR_DIR}/elama/php-codesniffer-rules/Elama_PHP${PHP_VERSION}/ruleset.xml \
+    cd ${WORKDIR}
+
+    ${BIN_DIR}/phpcs -p -v \
+    --standard=${WORKDIR}/vendor/elama/php-codesniffer-rules/Elama_PHP${PHP_VERSION}/ruleset.xml \
     --ignore=${IGNORED_DIRS} \
     --report=checkstyle \
     --extensions=php \
-    --report-file=${VENDOR_DIR}/../build/checkstyle-result.xml \
+    --report-file=${WORKDIR}/build/checkstyle-result.xml \
     --encoding=utf-8 \
     ${FILES}
 fi
