627dd266b9ad2eb12fda98292fccdba061df1377
Ilya <ilya.bulah@gmail.com>
2017-06-20 12:31:23 +0300

    invert FILES condition

--- scripts/run-codesniffer.sh
+++ scripts/run-codesniffer.sh
@@ -6,6 +6,11 @@ git fetch origin master:master || true
 fork_point=$(git merge-base --octopus master)
 FILES=$(git diff --diff-filter=AMRC --name-only ${fork_point} | grep .php | tr "\n" " ")
 
+if [ -z "$FILES" ]
+then
+    exit
+fi
+
 WORKDIR="`pwd`"
 
 cd $( dirname "$0" )
@@ -14,11 +19,9 @@ BIN_DIR="`pwd`"
 PHP_VERSION=$( php -r 'echo PHP_MAJOR_VERSION;' )
 IGNORED_DIRS=${1}
 
-if [ "$FILES" != "" ]
-then
-    cd "${WORKDIR}"
+cd "${WORKDIR}"
 
-    "${BIN_DIR}"/phpcs -p -v \
+"${BIN_DIR}"/phpcs -p -v \
     --standard="${WORKDIR}"/vendor/elama/php-codesniffer-rules/Elama_PHP${PHP_VERSION}/ruleset.xml \
     --ignore=${IGNORED_DIRS} \
     --report=checkstyle \
@@ -26,4 +29,3 @@ then
     --report-file="${WORKDIR}"/checkstyle-result.xml \
     --encoding=utf-8 \
     ${FILES}
-fi
